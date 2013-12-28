6fbf769dd8e8028d10fb133d921a2460fa6fab64
Kane Baccigalupi <baccigalupi@gmail.com>
2013-12-28 13:47:17 -0800

    railtie fix

--- lib/envar/railtie.rb
+++ lib/envar/railtie.rb
@@ -1,7 +1,7 @@
 module Envar
   class Railtie < Rails::Railtie
     initializer "envar.configure_rails_initialization" do
-      Envar::Config.environment = Rails.environment
+      Envar::Config.environment = Rails.env
     end
   end
 end
--- lib/envar/version.rb
+++ lib/envar/version.rb
@@ -1,3 +1,3 @@
 module Envar
-  VERSION = "0.1.3"
+  VERSION = "0.1.4"
 end
